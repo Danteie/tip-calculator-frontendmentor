8cdab221aed80b6c27a016fe20a30e9dc3b22f58
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-28 17:47:05 +0200

    addig display logic

--- src/Component/Calculator/Calculator.js
+++ src/Component/Calculator/Calculator.js
@@ -4,11 +4,17 @@ import { useState } from "react";
 
 export default function Calculator({test}) {
 
-  const [button,setButton] = useState();
+  const [button,setButton] = useState(15);
 
-  function buttonPress(){
-    const buttonValue = document.getElementsByTagName("button");
-    setButton(() => buttonValue[0].innerHTML)
+
+  function inputBill(event){
+    const inputText = event.target.value
+    console.log(inputText);
+  }
+
+  function buttonPress(event){
+    const buttonValue = event.target.innerHTML;
+    setButton(buttonValue)
     test(button)
   }
 
@@ -17,17 +23,17 @@ export default function Calculator({test}) {
     <div className="calculator">
       <div>
         <p>Bill</p>
-        <input type="text"/>
+        <input type="text" onChange={inputBill}/>
       </div>
       <div>
         <p>Select Tip %</p>
           <div className="parent">
               <div className="div1"><button onClick={buttonPress}>5%</button></div>
-              <div className="div2"> <button>10%</button></div>
-              <div className="div3"><button id="default">15%</button></div>
-              <div className="div4"><button>25%</button></div>
-              <div className="div5"><button>50%</button></div>
-              <div className="div6"><button>Custom</button></div>
+              <div className="div2"> <button onClick={buttonPress}>10%</button></div>
+              <div className="div3"><button onClick={buttonPress} id="default">15%</button></div>
+              <div className="div4"><button onClick={buttonPress}>25%</button></div>
+              <div className="div5"><button onClick={buttonPress}>50%</button></div>
+              <div className="div6"><button onClick={buttonPress}>Custom</button></div>
           </div>
       </div>
       <div>
